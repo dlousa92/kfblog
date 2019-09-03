c68af3c151efe87cfea3793ef4691785b9b01ee2
Daniel Lousa <dlousa92@gmail.com>
2019-09-03 12:43:04 -0400

    add links for previous and next posts

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,16 +1,20 @@
 import React from 'react'
-import {graphql} from 'gatsby'
+import {graphql, Link} from 'gatsby'
 
 const Template = (props) => {
   console.log(props)
+
+  const {next, prev} = props.pageContext
   const markdown = props.data.markdownRemark
   const title = markdown.frontmatter.title
   const content = markdown.html
   return (
     <div>
       <h1>{title}</h1>
-
       <div dangerouslySetInnerHTML={{__html: content}} />
+
+      {next && <Link to={next.frontmatter.path}>Next</Link>}
+      {prev && <Link to={prev.frontmatter.path}>Previous</Link>}
     </div>
   )
 }
