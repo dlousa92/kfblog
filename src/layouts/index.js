33f468786a685ae93cc44f9e553188918e29b4c6
Daniel Lousa <dlousa92@gmail.com>
2019-10-24 23:10:19 -0400

    create index.scss

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Header from '../components/Header/Header'
+import './index.scss'
 
 export default ({ children }) => (
-  <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
+  <div className='app' style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
     <Header />
     {children}
   </div>
