85d66dae08a800f53fc91d024c7d2ab40cf3c9ef
Daniel Lousa <dlousa92@gmail.com>
2019-11-07 21:41:06 -0500

    rename class

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,7 @@ import Header from '../components/Header/Header'
 import './index.scss'
 
 export default ({ children }) => (
-  <div className='app' style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
+  <div className='app'>
     <Header />
     {children}
   </div>
