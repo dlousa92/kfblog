0941fb18139d9e6205c8bd090e22e4fddd58f028
Daniel Lousa <dlousa92@gmail.com>
2019-11-13 23:31:20 -0500

    style post wrapper and include meta info

--- src/components/PostWrapper/PostWrapper.js
+++ src/components/PostWrapper/PostWrapper.js
@@ -7,15 +7,19 @@ const PostWrapper = (edges) => {
     <div className='post-wrapper'>
       {edges.edges.map(edge => {
         const { frontmatter, timeToRead } = edge.node
-        const tagClassName = frontmatter.tags[0].replace(/\s/g, '')
+        const tagClassName = frontmatter.tags[1].replace(/\s/g, '')
         console.log(frontmatter)
         console.log('time to read', timeToRead)
         return (
           <div key={frontmatter.path} className='post'>
-            <div className={`post-tag tag tag_${tagClassName}`}>{frontmatter.tags[0]}</div>
             <Link to={frontmatter.path} className='post-title'>
               {frontmatter.title}
             </Link>
+            <div className='post-info'>
+              <div className={`post-info_tag tag tag_${tagClassName}`}>{frontmatter.tags[1]}</div>
+              <p className='post-info_meta'>{frontmatter.date} &loz; {timeToRead} min read</p>
+            </div>
+            <div className='post-excerpt'>{frontmatter.excerpt}</div>
           </div>
         )
       })}
--- src/pages/2019-09-01-test-post/index.md
+++ src/pages/2019-09-01-test-post/index.md
@@ -4,7 +4,7 @@ date: "2019-09-01"
 title: "What's The Deal With Brobnar?"
 author: "Daniel Lousa"
 tags: ['Strategy', 'Analysis', 'News']
-excerpt: "This is a test post, enjoy"
+excerpt: "Let's breakdown what's changed with Brobnar in Worlds Collide"
 ---
 
 Hello my darling hello my baby hello my old time gal. Trying to see if this actually works
