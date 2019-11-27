a4b6b347846c5221cadc47be27264866fbe3d433
Daniel Lousa <dlousa92@gmail.com>
2019-11-26 21:32:25 -0500

    add sidebar and classes for styling

--- src/templates/singleTagIndex.js
+++ src/templates/singleTagIndex.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import PostWrapper from '../components/PostWrapper/PostWrapper'
+import Sidebar from '../components/Sidebar/Sidebar'
+import '../pages/index.scss'
 
 const SingleTagIndex = ({pageContext}) => {
   const { posts, tagName } = pageContext
   return (
-    <div>
-      <div>
-        Posts about {tagName}
-      </div>
-
-      <PostWrapper posts={posts} />
-    </div>
+    <>
+      <PostWrapper className='app-body_postwrapper' posts={posts} />
+      <Sidebar className='app-body_sidebar' />
+    </>
   )
 }
 
